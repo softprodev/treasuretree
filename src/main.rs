db6ebadc4f8c2e56dc809e14f01d18b53596eccf
SoftProDev <softprodev15@gmail.com>
2021-03-19 17:42:31 -0400

    treasure_qrcode

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-#![allow(unused)]
-
 #![feature(decl_macro, proc_macro_hygiene)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate include_dir;
 
 use std::path::{PathBuf, Path};
 use anyhow::{Result, anyhow, bail};
+use include_dir::Dir;
 use rocket::response::content::Html;
-use rocket_contrib::templates::Template;
-use serde::Serialize;
-use serde_json::json;
+    
+static STATIC_FILES: Dir = include_dir!("./static");
 
 #[get("/api/create")]
 fn create_treasure_key() -> String {
@@ -27,35 +26,69 @@ fn claim_treasure_with_key() -> String {
 }
 
 #[get("/")]
-fn root() -> Template {
-    Template::render("index", json!({}))
+fn root() -> Html<String> {
+    let file = STATIC_FILES.get_file("index.html").unwrap();
+    Html(file.contents_utf8().unwrap().to_string())
 }
 
 #[get("/<page>")]
-fn static_page(page: String) -> Template {
-    Template::render(page, json!({}))
+fn static_page(page: String) -> Result<Html<String>> {
+    let path = &Path::new(&page);
+    let path = path.with_extension("html");
+
+    let file = STATIC_FILES.get_file(&path)
+        .ok_or_else(|| anyhow!("not found"))?;
+
+    let content = file.contents_utf8().expect("utf8");
+
+    Ok(Html(content.to_owned()))
 }
 
-#[get("/css/<file>")]
-fn static_css(file: String) -> Template {
-    let file = &Path::new(&file);
+#[get("/<path..>", rank = 0)]
+fn static_file(path: PathBuf) -> Result<StaticResponder> {
 
-    assert!(file.file_name().is_some());
+    assert!(path.is_relative());
 
-    let template_name = file.with_extension("");
-    let template_name = format!("css/{}", template_name.display());
-    Template::render(template_name, json!({}))
+    let ext = path.extension()
+        .map(|ostr| ostr.to_str())
+        .flatten();
+
+    // HTML content is served from URI's with no file extension
+    if ext == Some("html") {
+        bail!("not found");
+    }
+
+    let file = STATIC_FILES.get_file(&path)
+        .ok_or_else(|| anyhow!("not found"))?;
+
+    let content = file.contents().to_vec();
+    let content_type = ext.map(|ext| ContentType::from_extension(ext))
+        .flatten()
+        .ok_or_else(|| anyhow!("can't determine content type"))?;
+
+    Ok(StaticResponder {
+        content,
+        content_type,
+    })
 }
+
+use rocket::http::ContentType;
+use rocket::response::Responder;
 
 mod treasure_qrcode;
 
+#[derive(Responder)]
+struct StaticResponder {
+    content: Vec<u8>,
+    content_type: ContentType,
+}
+
 fn main() {
     rocket::ignite()
-        .attach(Template::fairing())
         .mount("/", routes![
             root,
             static_page,
-            static_css,
+            static_file,
             create_treasure_key,
             plant_treasure_with_key,
             claim_treasure_with_key,
