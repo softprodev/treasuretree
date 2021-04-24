3a1a331712c61f7eb8cfc830705b34b97f9dadb3
SoftProDev <softprodev15@gmail.com>
2021-04-24 14:37:47 -0400

    Add loading indicators

--- static/js/create.js
+++ static/js/create.js
@@ -1,15 +1,42 @@
-console.log("before click");
 let createButton = document.getElementById("create-button");
-console.log(createButton);
+
+console.assert(createButton);
+
 createButton.addEventListener("click", async () => {
-    console.log("click");
 
-    let response = await fetch("api/create");
-    console.log(response);
+    let qrCodeContainer = document.getElementById("display-qrcode");
+    let secretKeyContainer = document.getElementById("secret-key");
+    let spinner = document.getElementById("create-spinner");
 
-    let jsonResponse = await response.json();
-    console.log(jsonResponse);
+    console.assert(qrCodeContainer);
+    console.assert(secretKeyContainer);
 
-    document.getElementById("display-qrcode").innerHTML = jsonResponse.qrcode;
+    qrCodeContainer.innerHTML = null;
+    secretKeyContainer.innerHTML = null;
+
+    createButton.disabled = true;
+    spinner.classList.remove("no-display");
+
+    try {
+        let response = await fetch("api/create");
+
+        console.log(response);
+
+        if (!response.ok) {
+            // TODO
+        }
+
+        let jsonResponse = await response.json();
+
+        console.log(jsonResponse);
+        console.assert(jsonResponse.qrcode);
+        console.assert(jsonResponse.secret_key);
+
+        qrCodeContainer.innerHTML = jsonResponse.qrcode;
+        secretKeyContainer.innerHTML = jsonResponse.secret_key;
+    } finally {
+        createButton.disabled = false;
+        spinner.classList.add("no-display");
+    }
 });
 
